434e543df897c6ec7a42b66bbc971690748211c7
aeagr-l <arthur.richards@bristol.ac.uk>
2023-11-24 08:35:54 +0000

    recording status now only shows relevant files

--- record.sh
+++ record.sh
@@ -1,8 +1,9 @@
 stream_id=0
+file_stub=video-$(date +"%Y-%m-%d-%H-%M-%S")
 cat sources.txt | while read line
 do
-  ffmpeg -v 0 $line -vcodec copy -an video-$(date +"%Y-%m-%d-%H-%M-%S")-src$stream_id.mkv &
+  ffmpeg -v 0 $line -vcodec copy -an $file_stub-src$stream_id.mkv &
   stream_id=$((stream_id+1))
 done
-watch 'ps -f | grep ffmpeg | grep -v grep; ls -l *.mkv'
+watch 'ps -f | grep ffmpeg | grep -v grep; ls -l '$file_stub'*.mkv'
 pkill ffmpeg
